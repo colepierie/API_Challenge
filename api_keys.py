2f91e3341d978b235d096043456004f76f390cbb
Cole B Pierie <69173513+colepierie@users.noreply.github.com>
2020-12-16 15:58:01 -0600

    final commit

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "95492305b01c5d01c810fd63e45b0cd5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCDLjw4Ae6GfbVrkV_dyqct_N0pgZzpVvE"
